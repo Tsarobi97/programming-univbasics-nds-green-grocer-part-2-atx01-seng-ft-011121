fd141753c37623b4f570960e3a6d479f5f654b99
Robert Obi <robobi97@googlemail.com>
2021-01-11 02:16:00 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,36 +1,25 @@
 require_relative './part_1_solution.rb'
 
-def find_item_by_name_in_collection(name, collection)
-  index = 0
-
-  collection.each do |grocery_item|
-    return grocery_item if grocery_item[:item] === name 
-    index += 1
-  end
-
-  nil
+def apply_coupons(cart, coupons)
+  # Consult README for inputs and outputs
+  #
+  # REMEMBER: This method **should** update cart
 end
 
+def apply_clearance(cart)
+  # Consult README for inputs and outputs
+  #
+  # REMEMBER: This method **should** update cart
+end
 
-def consolidate_cart(cart)
-  index = 0
-  new_cart = []
-  
-  cart.each do |grocery_item|
-    current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
-    if current_item
-      new_cart_index = 0
-      new_cart.each do |new_cart_item|
-        if new_cart_item[:item] === current_item[:item]
-          new_cart_item[:count] += 1
-        end
-        new_cart_index += 1
-      end
-    else
-      grocery_item[:count] = 1
-      new_cart << grocery_item
-    end
-    index += 1
-  end
-  new_cart
+def checkout(cart, coupons)
+  # Consult README for inputs and outputs
+  #
+  # This method should call
+  # * consolidate_cart
+  # * apply_coupons
+  # * apply_clearance
+  #
+  # BEFORE it begins the work of calculating the total (or else you might have
+  # some irritated customers
 end
